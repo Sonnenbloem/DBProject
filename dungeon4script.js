845883783cd889decdc0299f1e2e946dac57c2f5
sonnenbloem <sonnenblomst@gmail.com>
2017-08-16 23:52:36 +0200

    boh

---
+++ dungeon4script.js
@@ -0,0 +1,9 @@
+
+
+  $(document).ready(function(){
+    $( ".my-dices" ).click(function() {
+        $("button.part1").attr("hidden", true);
+        $("div.part2").removeAttr("hidden");
+
+  });
+});
